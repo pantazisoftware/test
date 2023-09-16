1508bc259595547f8c59113cd504b4e46f47f332
pantazisoftware <robert.pantazi02@gmail.com>
2023-09-16 22:15:18 +0300

    update resend

--- src/lib/email/mailer.ts
+++ src/lib/email/mailer.ts
@@ -1,12 +1,12 @@
+import { serverEnv } from '@/env/server';
 import { Resend } from 'resend';
 import MagicLinkEmail from './templates/magic-link';
-import { serverEnv } from '@/env/server';
 
 export const resend = new Resend(serverEnv.RESEND_SECRET);
 
 export async function sendMagicLinkEmail(to: string, signInLink: string) {
     const data = await resend.emails.send({
-        from: 'no-reply@saasplanet.app',
+        from: 'no-reply@mozocom.com',
         to: [to],
         subject: "Magic sign-in link",
         react: MagicLinkEmail({ signInLink: signInLink, sentTo: to }),
